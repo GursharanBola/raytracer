325e2f89934124921f95b3db1758415f2e90db71
Gursharan Bola <gursharanbola@berkeley.edu>
2026-06-05 10:50:04 -0700

    added a more complete render to README

--- include/camera.h
+++ include/camera.h
@@ -38,7 +38,7 @@ class camera {
     double aperture_radius = 0.02;
     double t_max;
     double t_min;
-    int num_samples = 500; // number of antialiasing samples.
+    int num_samples = 700; // number of antialiasing samples.
     int depth = 10;
     vec3 average_pixel_angular(int i, int j, double delta_theta,
                                double delta_phi,
